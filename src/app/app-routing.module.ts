26485efd0c616370d9238c206a1b1b860bcf11ef
Sunny Bhadani <sunnybhadani898@gmail.com>
2021-11-18 22:49:21 +0530

    Added Route Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './auth.service';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { AuthGuard } from './auth.guard';
+
 
 const routes: Routes = [
   {
@@ -27,6 +30,7 @@ const routes: Routes = [
   {
     path: 'reactive-form',
     component: ReactiveFormComponent,
+    canActivate: [AuthGuard] 
   },
   {
     path: '**',
@@ -36,6 +40,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
+
 })
 export class AppRoutingModule { }
